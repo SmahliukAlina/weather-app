937d9fec9bc9f9407aec3a90d960ea2aa3559c04
Alina Smahliuk <88776329+SmahliukAlina@users.noreply.github.com>
2022-05-08 20:56:33 +0300

    Update js file

--- src/index.js
+++ src/index.js
@@ -7,20 +7,25 @@ function getTemp(response) {
   let city = response.data.name;
   let temp = `${Math.round(response.data.main.temp)}°C`;
   let feelsLikeTemp = `${Math.round(response.data.main.feels_like)}°C`;
-  let currentCity = document.querySelector("#cur-city");
-  let currentTemp = document.querySelector("#cur-temp-value");
-  let currentFeelsLikeTemp = document.querySelector("#cur-feels-like-temp");
-  currentCity.innerHTML = city;
-  currentTemp.innerHTML = temp;
-  currentFeelsLikeTemp.innerHTML = feelsLikeTemp;
+
+  let cityElement = document.querySelector("#city");
+  let curTempElement = document.querySelector("#cur-temp-value");
+  let curFeelsLikeTempElement = document.querySelector("#cur-feels-like-temp");
+
+  cityElement.innerHTML = city;
+  curTempElement.innerHTML = temp;
+  curFeelsLikeTempElement.innerHTML = feelsLikeTemp;
 }
 
 function logPosition(position) {
   let latitude = Math.round(position.coords.latitude);
   let longitude = Math.round(position.coords.longitude);
+
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=${unitSys}&appid=${apiKey}`;
+
   axios.get(apiUrl).then(getTemp);
 }
+
 navigator.geolocation.getCurrentPosition(logPosition);
 
 //Button for searching current position
@@ -31,8 +36,6 @@ let currentLocButton = document.querySelector("#cur-loc-btn");
 currentLocButton.addEventListener("click", getCurPos);
 
 //Set current date
-let now = new Date();
-
 let weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 let months = [
   "Jan",
@@ -49,20 +52,6 @@ let months = [
   "Dec",
 ];
 
-let currentWeekDay = weekDays[now.getDay()];
-let currentDate = now.getDate();
-let currentMonth = months[now.getMonth()];
-let currentYear = now.getFullYear();
-
-let curWeekDay = document.querySelector("#cur-weekday");
-curWeekDay.innerHTML = `${currentWeekDay}`;
-let fullCurrentDate = document.querySelector("#cur-date");
-fullCurrentDate.innerHTML = `${currentMonth} ${currentDate}, ${currentYear}`;
-
-let currentHour = now.getHours();
-let currentMin = now.getMinutes();
-let currentTime = document.querySelector("#cur-time");
-
 function getTime(hour, min) {
   if (min < 10) {
     return `${hour}:0${min}`;
@@ -71,39 +60,57 @@ function getTime(hour, min) {
   }
 }
 
+let now = new Date();
+let currentWeekDay = weekDays[now.getDay()];
+let currentDate = now.getDate();
+let currentMonth = months[now.getMonth()];
+let currentYear = now.getFullYear();
+let currentHour = now.getHours();
+let currentMin = now.getMinutes();
+
+let curWeekDayElement = document.querySelector("#cur-weekday");
+let fullCurDateElement = document.querySelector("#cur-date");
+let currentTime = document.querySelector("#cur-time");
+
+curWeekDayElement.innerHTML = `${currentWeekDay}`;
+fullCurDateElement.innerHTML = `${currentMonth} ${currentDate}, ${currentYear}`;
 currentTime.innerHTML = getTime(currentHour, currentMin);
 
 //Search and Change current city
 function changeCity(event) {
   event.preventDefault();
-  let searchCityInput = document.querySelector("#search-city-input");
-  if (searchCityInput.value !== "") {
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCityInput.value}&units=${unitSys}&appid=${apiKey}`;
+
+  let searchCityInputElem = document.querySelector("#search-city-input");
+
+  if (searchCityInputElem.value !== "") {
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCityInputElem.value}&units=${unitSys}&appid=${apiKey}`;
     axios.get(apiUrl).then(getTemp);
-    searchCityInput.value = null;
+
+    searchCityInputElem.value = null;
   }
 }
 
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", changeCity);
+let searchFormElement = document.querySelector("#search-form");
+
+searchFormElement.addEventListener("submit", changeCity);
 
 //Change temperature units
 let tempCelsiusValue = 22;
 let tempFarenhValue = (tempCelsiusValue * 9) / 5 + 32;
 
 function changeUnit() {
-  let changeUnitButton = document.querySelector("#change-unit-btn");
-  let currentTempValue = document.querySelector("#cur-temp-value");
-  if (changeUnitButton.value === "°F") {
-    changeUnitButton.innerHTML = "°C";
-    currentTempValue.innerHTML = `${tempFarenhValue}°F`;
-    changeUnitButton.value = "°C";
+  let changeUnitBtnElement = document.querySelector("#change-unit-btn");
+  let curTempValueElement = document.querySelector("#cur-temp-value");
+  if (changeUnitBtnElement.value === "°F") {
+    changeUnitBtnElement.innerHTML = "°C";
+    curTempValueElement.innerHTML = `${tempFarenhValue}°F`;
+    changeUnitBtnElement.value = "°C";
   } else {
-    changeUnitButton.innerHTML = "°F";
-    currentTempValue.innerHTML = `${tempCelsiusValue}°C`;
-    changeUnitButton.value = "°F";
+    changeUnitBtnElement.innerHTML = "°F";
+    curTempValueElement.innerHTML = `${tempCelsiusValue}°C`;
+    changeUnitBtnElement.value = "°F";
   }
 }
 
-let changeUnitButton = document.querySelector("#change-unit-btn");
-changeUnitButton.addEventListener("click", changeUnit);
+let changeUnitBtnElemen = document.querySelector("#change-unit-btn");
+changeUnitBtnElemen.addEventListener("click", changeUnit);
